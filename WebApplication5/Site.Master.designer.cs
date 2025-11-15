f1c91a0a0c001ae051a2dee0f4435f469a74ab49
mtmyers5 <19690mtm@gmail.com>
2025-11-15 09:33:40 -0700

    Added a Welcome {user} message to the Nav bar. Also I updated how Cookies authencate users to help with grabbing the correct session user information.

--- WebApplication5/Site.Master.designer.cs
+++ WebApplication5/Site.Master.designer.cs
@@ -24,6 +24,15 @@ namespace WebApplication5
         protected global::System.Web.UI.ScriptManager ScriptManager1;
 
         /// <summary>
+        /// WelcomeLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label WelcomeLabel;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
